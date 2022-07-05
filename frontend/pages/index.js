6beb1fd0681e6ab9ec68a4f6047729abfc167bd6
Anthony Williams <hi@antw.dev>
2022-07-05 12:16:37 +0100

    Remove Tailwind example

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -30,31 +30,6 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className="mt-6 text-lg">
-          <Link href="tailwind-example">
-            <a
-              href="tailwind-example"
-              className="group flex items-center gap-0.5 p-3 hover:no-underline"
-            >
-              <Fragment>
-                <span className="group-hover:underline">See an example with Tailwind</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 transition group-hover:translate-x-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </Fragment>
-            </a>
-          </Link>
-        </p>
-
         <p className="my-6 text-center text-xl">
           Get started by editing{" "}
           <code className="rounded bg-gray-100 px-2 py-1">pages/index.js</code>
